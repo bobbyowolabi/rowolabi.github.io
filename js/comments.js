51cf2c531087c574e21ad711daf646eabc73b05d
Bobby Owolabi <bobbyowolabi@gmail.com>
2017-05-14 17:16:13 -0400

    Adding disqus comments

---
+++ js/comments.js
@@ -0,0 +1,10 @@
+var disqus_config = function () {
+this.page.url = http://www.bobbyowolabi.com{{ page.url }};
+this.page.identifier = {{ page.id }};
+};
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://bobbyowolabi.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
